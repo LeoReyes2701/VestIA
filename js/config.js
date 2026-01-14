f545078382670ab382e9a1fccad76e48011eee8d
Lejomigo <leopoldomicettgonzalez@gmail.com>
2026-01-14 08:07:26 -0400

    Todo en orden, cambios tontos en el html

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const API_KEY = "TU_CLAVE_AQUI";
+export const API_KEY = "TU API KEY AQUÍ";
 export const GEMINI_MODEL = "gemini-2.5-flash"; 
 export const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent`;
